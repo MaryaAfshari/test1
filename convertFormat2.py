2b69e163d7a1285f204789256fe2f87e6c9a6f41
afshari-maryam <afshari1431@gmail.com>
2023-09-21 05:03:41 +0300

    add another format function using pydub module

---
+++ convertFormat2.py
@@ -0,0 +1,15 @@
+from pydub import AudioSegment
+
+def convert_m4a_to_wav(input_file, output_file):
+    try:
+        audio = AudioSegment.from_file(input_file, format='m4a')
+        audio.export(output_file, format='wav')
+        print(f"Conversion successful: {output_file}")
+    except Exception as e:
+        print(f"Conversion failed: {e}")
+
+# Example usage
+input_file = '/mnt/disk1/data/voxceleb_2/dev/aac/id00012/21Uxsk56VDQ/00001.m4a'
+output_file = 'output.wav'
+
+convert_m4a_to_wav(input_file, output_file)
