9d40a223ab8d1204ee3707e63952e4d2f0fd5090
afshari-maryam <afshari1431@gmail.com>
2023-09-22 18:59:18 +0300

    add some python files for making train , validation and test lists v2 and v3 and clear text files before writing in it

---
+++ makeListsDeepmine-V3.py
@@ -0,0 +1,76 @@
+import os
+import random
+
+# Specify the path to the main dataset folder
+#dataset_folder = "mnt/disk1/data/DeepMine/wav"
+dataset_folder =  "/Users/maryamafshari/Desktop/Thesis_data/Deepmine Small Sample/SampleDeepMine/wav"
+
+
+# Define split ratios
+train_ratio = 0.7
+val_ratio = 0.15
+test_ratio = 0.15
+
+# Clear existing text files before writing
+def clear_file(file_path):
+    with open(file_path, "w") as file:
+        file.truncate(0)
+
+# Function to write a list to a file
+def write_list_to_file(data_list, file_path):
+    with open(file_path, "a") as file:
+        for example in data_list:
+            speaker_id, session_id, file_path = example
+            file.write(f"{speaker_id} {session_id} {file_path}\n")
+
+# Create empty lists to store train, validation, and test lists
+train_list = []
+val_list = []
+test_list = []
+
+# Iterate over the speaker directories
+for speaker_dir in os.listdir(dataset_folder):
+    speaker_path = os.path.join(dataset_folder, speaker_dir)
+    if not os.path.isdir(speaker_path):
+        continue
+    
+    # Iterate over the session directories
+    for session_dir in os.listdir(speaker_path):
+        session_path = os.path.join(speaker_path, session_dir)
+        if not os.path.isdir(session_path):
+            continue
+        
+        # Iterate over the wave files in the session directory
+        for filename in os.listdir(session_path):
+            if filename.endswith(".wav"):
+                # Extract the speaker ID, session ID, and file path
+                speaker_id = speaker_dir
+                session_id = session_dir
+                file_path = os.path.join(session_path, filename)
+                
+                # Randomly assign the example to train, validation, or test list
+                rand_value = random.random()
+                if rand_value < train_ratio:
+                    train_list.append((speaker_id, session_id, file_path))
+                elif rand_value < train_ratio + val_ratio:
+                    val_list.append((speaker_id, session_id, file_path))
+                else:
+                    test_list.append((speaker_id, session_id, file_path))
+
+# Clear existing text files before writing
+train_file_path = "train_list.txt"
+val_file_path = "val_list.txt"
+test_file_path = "test_list.txt"
+
+clear_file(train_file_path)
+clear_file(val_file_path)
+clear_file(test_file_path)
+
+# Write train list to a file
+write_list_to_file(train_list, train_file_path)
+
+# Write validation list to a file
+write_list_to_file(val_list, val_file_path)
+
+# Write test list to a file
+write_list_to_file(test_list, test_file_path)
